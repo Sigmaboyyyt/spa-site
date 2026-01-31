6434b74c6b8033f41d504620331ad2e6b6f71ba7
Sigmaboyyyt <vffgeg4@gmail.com>
2026-01-31 20:24:18 +0400

    images

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+
+print("STATIC FOLDER PATH:", app.static_folder)
+print("TEMPLATE FOLDER PATH:", app.template_folder)
+print("CURRENT WORK DIR:", os.getcwd())
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
